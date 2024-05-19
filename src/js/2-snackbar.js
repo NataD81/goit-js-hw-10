0a4af5fde9cd593a9fc673476159b8166b401929
NataD81 <dvoryanchuk81@gmail.com>
2024-05-19 23:02:27 +0300

    go snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,45 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const submitBtn = form.querySelector('button[type="submit"]');
+
+function createNotificationPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
+
+submitBtn.addEventListener('click', event => {
+  event.preventDefault();
+
+  const delayInput = form.querySelector('input[name="delay"]');
+  const stateInput = form.querySelector('input[name="state"]:checked');
+
+  const delay = parseInt(delayInput.value);
+  const state = stateInput.value;
+
+  const notificationPromise = createNotificationPromise(delay, state);
+
+  notificationPromise.then(
+    delay => {
+      iziToast.success({
+        title: 'Fulfilled promise',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    },
+
+    delay => {
+      iziToast.error({
+        title: 'Rejected promise',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    }
+  );
+});
